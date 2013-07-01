50d1b962e7d72db404530f420e94ccc173f63add
Baptiste Gaillard <baptiste.gaillard@gomoob.com>
2013-07-01 18:12:13 +0200

    Revert "Revert "First Commit for the GOMOOB Fork""
    This reverts commit 97d3ddbca006f80b185c70dcc501402bb515ea0f.

--- src/PhlyRestfully/Link.php
+++ src/PhlyRestfully/Link.php
@@ -14,37 +14,89 @@ use Zend\Uri\Exception as UriException;
 use Zend\Uri\UriFactory;
 
 /**
- * Object describing a link relation
+ * Class which describe a HAL Link.
+ *
+ * <p>To be compliant with the HAL Specifications a HAL Link should be able to contain the following attributes:</p>
+ * <ul>
+ *     <li>href      : For indicating the target URI. href corresponds with the ’Target IRI’ as defined in Web Linking
+ *                     (RFC 5988). This attribute MAY contain a URI Template (RFC6570) and in which case, SHOULD be
+ *                     complemented by an additional templated attribtue on the link with a boolean value true.</li>
+ *     <li>rel       : For identifying how the target URI relates to the 'Subject Resource'. The Subject Resource is the
+ *                     closest parent Resource element. This attribute is not a requirement for the root element of a
+ *                     HAL representation, as it has an implicit default value of 'self'. rel corresponds with the
+ *                     'relation parameter' as defined in Web Linking (RFC 5988). rel attribute SHOULD be used for
+ *                     identifying Resource and Link elements in a HAL representation.</li>
+ *     <li>name      : For distinguishing between Resource and Link elements that share the same rel value. The name
+ *                     attribute SHOULD NOT be used exclusively for identifying elements within a HAL representation, it
+ *                     is intended only as a ‘secondary key’ to a given rel value.</li>
+ *     <li>hreflang  : For indicating what the language of the result of dereferencing the link should be.</li>
+ *     <li>title     : For labeling the destination of a link with a human-readable identifier.</li>
+ *     <li>templated : This attribute SHOULD be present with a boolean value of true when the href of the link contains
+ *                     a URI Template (RFC6570).</li>
+ *
+ * </ul>
+ *
+ * @author Baptiste GAILLARD (baptiste.gaillard@gomoob.com)
+ * @see http://stateless.co/hal_specification.html
+ * @see http://tools.ietf.org/html/rfc5988
+ * @see http://tools.ietf.org/html/rfc6570
  */
 class Link
 {
+    /**
+     * The 'templated' HAL Link attribute.
+     *
+     * <p>This attribute SHOULD be present with a boolean value of true when the href of the link contains a URI
+     *    Template (RFC6570).</p>
+     *
+     * @var string
+     */
+    private $templated = null;
+
+    /**
+     * The 'title' HAL Link attribute.
+     *
+     * <p>The title is used for labeling the destination of a link with a human-readable identifier.</p>
+     *
+     * @var string
+     */
+    private $title = null;
+
     /**
      * @var string
      */
     protected $relation;
 
     /**
+     * TODO: DOCUMENTATION...
+     *
      * @var string
      */
     protected $route;
 
     /**
+     * TODO: DOCUMENTATION...
+     *
      * @var array
      */
     protected $routeOptions = array();
 
     /**
+     * TODO: DOCUMENTATION...
+     *
      * @var array
      */
     protected $routeParams = array();
 
     /**
+     * TODO: DOCUMENTATION...
+     *
      * @var string
      */
     protected $url;
 
     /**
-     * Create a link relation
+     * Create a link relation.
      *
      * @todo  filtering and/or validation of relation string
      * @param string $relation
@@ -52,6 +104,33 @@ class Link
     public function __construct($relation)
     {
         $this->relation = (string) $relation;
+    }
+
+    /**
+     * Gets the 'title' HAL Link attribute.
+     *
+     * <p>The title is used for labeling the destination of a link with a human-readable identifier.</p>
+     *
+     * @return string the 'title' HAL Link attribute.
+     */
+    public function getTitle() {
+
+        return $this -> title;
+
+    }
+
+    /**
+     * Indicates is the HAL Link is templated.
+     *
+     * <p>This attribute SHOULD be present with a boolean value of true when the href of the link contains a URI
+     *    Template (RFC6570).</p>
+     *
+     * @return string <code>true</tt> if the link is templated, <code>false</tt> otherwise.
+     */
+    public function isTemplated() {
+
+        return $this -> templated;;
+
     }
 
     /**
@@ -129,6 +208,33 @@ class Link
 
         $this->routeParams = $params;
         return $this;
+    }
+
+    /**
+     * Sets the 'templated' HAL Link attribute.
+     *
+     * <p>This attribute SHOULD be present with a boolean value of true when the href of the link contains a URI
+     *    Template (RFC6570).</p>
+     *
+     * @param unknown $templated the 'templated' HAL Link attribute to set.
+     */
+    public function setTemplated($templated) {
+
+        $this -> templated = $templated;
+
+    }
+
+    /**
+     * Sets the 'title' HAL Link attribute.
+     *
+     * <p>The title is used for labeling the destination of a link with a human-readable identifier.</p>
+     *
+     * @param unknown $title the 'title' HAL Link attribute to set.
+     */
+    public function setTitle($title) {
+
+        $this -> title = $title;
+
     }
 
     /**
